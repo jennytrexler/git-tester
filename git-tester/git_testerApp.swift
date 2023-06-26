2db077bccff3215fbd73223451597676f720ff9a
Jenny Trexler <jennytrexler@gmail.com>
2023-06-26 10:22:23 -0700

    Initial Commit

---
+++ git-tester/git_testerApp.swift
@@ -0,0 +1,17 @@
+//
+//  git_testerApp.swift
+//  git-tester
+//
+//  Created by Jenny Trexler on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct git_testerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
